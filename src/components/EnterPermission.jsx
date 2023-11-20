1f52e5ad88d5347c69bcc8126ccbb08ae5b4caf0
bizzome <bizzo.mateus@gmail.com>
2023-11-20 18:42:00 -0300

    feat: add link and changes component style
    The component style is changing a little and now there is a real conditional challenge to get to github.

--- src/components/EnterPermission.jsx
+++ src/components/EnterPermission.jsx
@@ -1,8 +1,10 @@
 export default function EnterPermission({ counter }) {
     return (
-        <div className='challenge-block'>
-            <button className="card" disabled>Can you enter here?</button>
-            <p className="card">You only clicked {counter} times.</p>
+        <div className='challenge-block'> 
+            <span className="card" disabled><p>Can you guess?</p> <input type="text" name="guess" />
+                <p>You only clicked { counter } times.</p>
+                {counter == 32? <><h1>OMG!! YOU GUESSED!!</h1><a href='https://github.com/bizzome/react-project/'>Give me a star!</a></>:null }
+            </span>
         </div>
     )
   }
